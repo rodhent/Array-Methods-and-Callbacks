8583f3e3e10af417bff038932b71b110e742c3cb
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-08-18 18:22:52 -0400

    javascript update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ console.log('its working');
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(/* code here * /) {
 
     /* code here */
 
